1f42aeb988775f46e770eec86e94c50faa380290
Matt Durham <mattdurham@ppog.org>
2017-06-04 12:07:43 -0400

    Done week 6 where we replaced the SQL with entity framework

--- Contacto/Contacto/Controllers/HomeController.cs
+++ Contacto/Contacto/Controllers/HomeController.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Dapper;
+using Contacto.Repositories;
 
 namespace Contacto.Controllers
 {
     public class HomeController : Controller
     {
-        static ContactRepository _contactRepository = new ContactRepository();
+
+        
         public ActionResult Index()
         {
             return View();
@@ -39,69 +41,39 @@ namespace Contacto.Controllers
 
         public ActionResult AddContact(Contact contact)
         {
-            _contactRepository.AddContact(contact);
-            return View("SingleContact",contact);
+            using (var repo = new ContactRepository())
+            {
+                repo.AddContact(contact);
+                return View("SingleContact", contact);
+            }
         }
 
 
         public ActionResult ViewContacts()
         {
-            return View("ContactList", _contactRepository.Contacts);
+            using (var repo = new ContactRepository())
+            {
+                return View("ContactList", repo.Contacts.ToList());
+            }
         }
 
         public ActionResult EditContact(int id)
         {
-            return View("EditContact", _contactRepository.GetContact(id));
+            using (var repo = new ContactRepository())
+            {
+                return View("EditContact", repo.GetContact(id));
+            }
         }
 
         public ActionResult ChangeContact(Contact contact)
         {
-            _contactRepository.EditContact(contact);
-            return View("ContactList", _contactRepository.Contacts);
+            using (var repo = new ContactRepository())
+            {
+                repo.EditContact(contact);
+                return View("ContactList", repo.Contacts.ToList());
+            }
         }
     }
 
-    public class ContactRepository
-    {
-
-        public void AddContact(Contact contact)
-        {
-            using (var connection = CreateConnection())
-            {
-                connection.Execute("INSERT INTO CONTACTS (LASTNAME, FIRSTNAME) VALUES (@lastname, @firstname)", new { lastname = contact.LastName, firstname = contact.FirstName });
-            }
-        }
-
-        public List<Contact> Contacts
-        {
-            get
-            {
-                using (var connection = CreateConnection())
-                {
-                    return connection.Query<Contact>("SELECT * FROM CONTACTS").ToList();
-                }
-            }
-        }
-
-        public Contact GetContact(int id)
-        {
-            using (var connection = CreateConnection())
-            {
-                return connection.QuerySingle<Contact>("SELECT * FROM CONTACTS WHERE ID = @contactid", new { contactid = id });
-            }
-        }
-
-        public void EditContact(Contact contact)
-        {
-            using (var connection = CreateConnection())
-            {
-                connection.Execute("UPDATE CONTACTS SET LASTNAME = @lastname, FIRSTNAME = @firstname where ID = @id", new { lastname = contact.LastName, firstname = contact.FirstName, id = contact.ID });
-            }
-        }
-
-        private IDbConnection CreateConnection()
-        {
-            return new SqlConnection("Server=(localdb)\\mssqllocaldb;Database=Contacto;Trusted_Connection=True");
-        }
-    }
+   
 }
---
+++ Contacto/Contacto/Repositories/ContactRepository.cs
@@ -0,0 +1,45 @@
+﻿using Contacto.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Contacto.Repositories
+{
+    public class ContactRepository : DbContext
+    {
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ContactRepository() : base("ContactRepository")
+        {
+            Debug.Write(Database.Connection.ConnectionString);
+        }
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+            this.SaveChanges();
+        }
+
+        public Contact GetContact(int id)
+        {
+            //This is a LINQ query that returns the contact
+            //  where the id matches and to pull the first one
+            //  NOTE if you pass an id that doesn't exist this will error
+            return (from x in Contacts
+             where x.ID == id
+             select x).First();
+        }
+
+        public void EditContact(Contact contact)
+        {
+            //This tells the DbContext to associate contact with a database record 
+            //  and mark it as modified so that when save changes is called to update the database
+            this.Entry(contact).State = EntityState.Modified;
+            this.SaveChanges();
+        }
+    }
+}
